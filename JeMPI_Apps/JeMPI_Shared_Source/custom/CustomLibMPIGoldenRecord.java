f82d5347c61216720e0b30f603be4d0b6fe1322a
William Hayes <william.hayes@jembi.org>
2023-02-08 20:13:40 +0200

    switch to nigeria example

--- JeMPI_Apps/JeMPI_Shared_Source/custom/CustomLibMPIGoldenRecord.java
+++ JeMPI_Apps/JeMPI_Shared_Source/custom/CustomLibMPIGoldenRecord.java
@@ -11,25 +11,21 @@ import org.jembi.jempi.shared.models.CustomGoldenRecord;
 record CustomLibMPIGoldenRecord (@JsonProperty("uid") String uid,
                                  @JsonProperty("GoldenRecord.source_id") List<LibMPISourceId> sourceId,
                                  @JsonProperty("GoldenRecord.aux_id") String auxId,
-                                 @JsonProperty("GoldenRecord.given_name") String givenName,
-                                 @JsonProperty("GoldenRecord.family_name") String familyName,
+                                 @JsonProperty("GoldenRecord.nat_fingerprint_code") String natFingerprintCode,
+                                 @JsonProperty("GoldenRecord.emr_fingerprint_code") String emrFingerprintCode,
                                  @JsonProperty("GoldenRecord.gender") String gender,
                                  @JsonProperty("GoldenRecord.dob") String dob,
-                                 @JsonProperty("GoldenRecord.city") String city,
-                                 @JsonProperty("GoldenRecord.phone_number") String phoneNumber,
-                                 @JsonProperty("GoldenRecord.national_id") String nationalId) {
+                                 @JsonProperty("GoldenRecord.city") String city) {
 
    CustomLibMPIGoldenRecord(final CustomLibMPIDGraphEntity dgraphEntity) {
       this(null,
            List.of(dgraphEntity.sourceId()),
            dgraphEntity.auxId(),
-           dgraphEntity.givenName(),
-           dgraphEntity.familyName(),
+           dgraphEntity.natFingerprintCode(),
+           dgraphEntity.emrFingerprintCode(),
            dgraphEntity.gender(),
            dgraphEntity.dob(),
-           dgraphEntity.city(),
-           dgraphEntity.phoneNumber(),
-           dgraphEntity.nationalId());
+           dgraphEntity.city());
    }
 
    CustomGoldenRecord toCustomGoldenRecord() {
@@ -38,13 +34,11 @@ record CustomLibMPIGoldenRecord (@JsonProperty("uid") String uid,
                                       ? this.sourceId().stream().map(LibMPISourceId::toSourceId).toList()
                                       : List.of(),
                                     this.auxId(),
-                                    this.givenName(),
-                                    this.familyName(),
+                                    this.natFingerprintCode(),
+                                    this.emrFingerprintCode(),
                                     this.gender(),
                                     this.dob(),
-                                    this.city(),
-                                    this.phoneNumber(),
-                                    this.nationalId());
+                                    this.city());
    }
 
 }
--- JeMPI_Apps/JeMPI_Shared_Source/custom/CustomLibMPIMutations.java
+++ JeMPI_Apps/JeMPI_Shared_Source/custom/CustomLibMPIMutations.java
@@ -15,24 +15,20 @@ class CustomLibMPIMutations {
          """
          _:%s  <Entity.source_id>                <%s>        .
          _:%s  <Entity.aux_id>                   %s          .
-         _:%s  <Entity.given_name>               %s          .
-         _:%s  <Entity.family_name>              %s          .
+         _:%s  <Entity.nat_fingerprint_code>     %s          .
+         _:%s  <Entity.emr_fingerprint_code>     %s          .
          _:%s  <Entity.gender>                   %s          .
          _:%s  <Entity.dob>                      %s          .
          _:%s  <Entity.city>                     %s          .
-         _:%s  <Entity.phone_number>             %s          .
-         _:%s  <Entity.national_id>              %s          .
          _:%s  <dgraph.type>                     "Entity"    .
          """,
          uuid, sourceIdUid,
          uuid, AppUtils.quotedValue(customEntity.auxId()),
-         uuid, AppUtils.quotedValue(customEntity.givenName()),
-         uuid, AppUtils.quotedValue(customEntity.familyName()),
+         uuid, AppUtils.quotedValue(customEntity.natFingerprintCode()),
+         uuid, AppUtils.quotedValue(customEntity.emrFingerprintCode()),
          uuid, AppUtils.quotedValue(customEntity.gender()),
          uuid, AppUtils.quotedValue(customEntity.dob()),
          uuid, AppUtils.quotedValue(customEntity.city()),
-         uuid, AppUtils.quotedValue(customEntity.phoneNumber()),
-         uuid, AppUtils.quotedValue(customEntity.nationalId()),
          uuid);
    }
 
@@ -45,25 +41,21 @@ class CustomLibMPIMutations {
          """
          _:%s  <GoldenRecord.source_id>                     <%s>             .
          _:%s  <GoldenRecord.aux_id>                        %s               .
-         _:%s  <GoldenRecord.given_name>                    %s               .
-         _:%s  <GoldenRecord.family_name>                   %s               .
+         _:%s  <GoldenRecord.nat_fingerprint_code>          %s               .
+         _:%s  <GoldenRecord.emr_fingerprint_code>          %s               .
          _:%s  <GoldenRecord.gender>                        %s               .
          _:%s  <GoldenRecord.dob>                           %s               .
          _:%s  <GoldenRecord.city>                          %s               .
-         _:%s  <GoldenRecord.phone_number>                  %s               .
-         _:%s  <GoldenRecord.national_id>                   %s               .
          _:%s  <GoldenRecord.entity_list>                   <%s> (score=%f)  .
          _:%s  <dgraph.type>                                "GoldenRecord"   .
          """,
          uuid, sourceUid,
          uuid, AppUtils.quotedValue(customEntity.auxId()),
-         uuid, AppUtils.quotedValue(customEntity.givenName()),
-         uuid, AppUtils.quotedValue(customEntity.familyName()),
+         uuid, AppUtils.quotedValue(customEntity.natFingerprintCode()),
+         uuid, AppUtils.quotedValue(customEntity.emrFingerprintCode()),
          uuid, AppUtils.quotedValue(customEntity.gender()),
          uuid, AppUtils.quotedValue(customEntity.dob()),
          uuid, AppUtils.quotedValue(customEntity.city()),
-         uuid, AppUtils.quotedValue(customEntity.phoneNumber()),
-         uuid, AppUtils.quotedValue(customEntity.nationalId()),
          uuid, entityUid, score,
          uuid);
    }
