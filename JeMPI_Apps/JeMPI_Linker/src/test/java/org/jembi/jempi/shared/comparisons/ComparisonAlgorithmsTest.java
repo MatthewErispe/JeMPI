6321f08f6ca00e4dfcbace9c8af006307c602038
BruceMD <bruce.dickie@jembi.org>
2023-03-09 17:25:27 +0200

    Creating a test file that I cannot view for some reason...

--- JeMPI_Apps/JeMPI_Linker/src/test/java/org/jembi/jempi/shared/comparisons/ComparisonAlgorithmsTest.java
+++ JeMPI_Apps/JeMPI_Linker/src/test/java/org/jembi/jempi/shared/comparisons/ComparisonAlgorithmsTest.java
@@ -1,12 +1,14 @@
 package org.jembi.jempi.shared.comparisons;
 
+import org.apache.commons.text.similarity.JaroWinklerSimilarity;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
+
 class ComparisonAlgorithmsTest {
 
     @Test
     void compareJaroWinkler() {
+        assert true;
     }
 
     @Test
